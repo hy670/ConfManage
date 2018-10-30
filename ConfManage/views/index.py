048b907b571b6503088ec704c7378501f9f1efa8
hy670 <hy670@163.com>
2018-10-30 22:20:56 +0800

    1021

--- ConfManage/views/index.py
+++ ConfManage/views/index.py
@@ -8,10 +8,12 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
-
+from ConfManage.utils.graph import topology
 
 def index(request):
-    return render(request,'index.html')
+    nodelist =topology.node()
+    edges = topology.edges()
+    return render(request,'index.html', {'nodelist': nodelist,'edges':edges})
 
 def login(request):
     if request.session.get('username') is not None:
