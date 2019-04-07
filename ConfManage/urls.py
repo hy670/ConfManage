bc30fa13bf9816b6712a1bde2a50e3ab808e8610
hy670 <hy670@163.com>
2019-04-07 21:46:31 +0800

    328

--- ConfManage/urls.py
+++ ConfManage/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from ConfManage.views import (index, policy, users, assets, wssh, conffile, topo)
+from ConfManage.views import (index, policy, users, assets, wssh, conffile, topo, conf_bak)
 
 urlpatterns = [
 	url(r'^$', index.index),
