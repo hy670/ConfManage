66e9aa739172578ec9805103bf30c0c4606d56b7
HY <hy670@163.com>
2019-04-23 18:09:17 +0800

    419

--- baseline/urls.py
+++ baseline/urls.py
@@ -3,6 +3,7 @@ from .views import check_rule
 
 urlpatterns = [
 	url(r'^RegexGroupLis/$', check_rule.check_rule),
+	url(r'^RegexRuleDetail/$', check_rule.regex_rule_detail),
 	url(r'^RegexGroupDetail/(?P<group_id>[0-9]+)$', check_rule.regex_group_detail),
 	url(r'^RegexGroupAdd/$', check_rule.regex_group_add),
 
--- baseline/views/check_rule.py
+++ baseline/views/check_rule.py
@@ -6,19 +6,20 @@ from django.db import transaction
 from baseline.models import *
 from django.http import Http404
 from django.contrib.auth.decorators import permission_required
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from ConfManage.utils.logger import logger
 import json
-from django.core import serializers
+from ConfManage import serializers
 
 def check_rule(request):
 	if request.method == "GET":
 		regexgrouplist = check_group_mge.objects.all()
-		print(regexgrouplist)
+
 		return render(request, 'baseline/RegexGroupLis.html', {"regexgrouplist": regexgrouplist})
 
 
 def regex_group_detail(request, group_id):
+	print(request.method)
 	if request.method == "GET":
 		group = check_group_mge.objects.get(group_id=group_id)
 		group_rule_detail_list = check_grp_rel.objects.filter(group=group)
@@ -27,6 +28,33 @@ def regex_group_detail(request, group_id):
 			rule = group_rule_detail.rule
 			rulelist.append(rule)
 		return render(request, 'baseline/RegexGroupDetail.html', {'group': group, 'rulelist': rulelist})
+
+
+def regex_rule_detail(request):
+	print(request.method)
+	if request.method == "GET":
+		pass
+		return render(request, 'baseline/RegexGroupDetail.html')
+	elif request.method == "POST":
+		rule_id = int(request.POST.get('rule_id'))
+		print(rule_id)
+		try:
+			rulecontentlist =[]
+			rule = serializers.RuleManageSerializer(check_rule_manage.objects.get(rule_id=rule_id)).data
+			rulecontent = check_rule_content.objects.filter(rule_id=rule_id)
+			print(type(rule))
+			print(rule)
+			for i in rulecontent:
+				data = serializers.RuleContentSerializer(i)
+				rulecontentlist.append(data.data)
+			return JsonResponse({'code': 400, 'rule':  rule, 'rulecontent':  rulecontentlist})
+		except Exception as e:
+			logger.warn("查询失败失败原因：%s", e)
+
+
+		#return JsonResponse({'code':500,'rule': rule, 'rulecontent': rulecontent})
+
+
 
 
 def regex_group_add(request):
